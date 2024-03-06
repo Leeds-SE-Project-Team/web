70b79d492e7827491f2aeaf505d3a13a6f5f1099
wmzspace <sc21m2w@leeds.ac.uk>
2024-03-06 16:00:59 +0800

    feat: update mobile ui

--- src/components/mobile/navigation/FooterNav.vue
+++ src/components/mobile/navigation/FooterNav.vue
@@ -1,4 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
 
-<template><div>Footer</div></template>
+const active = ref('discover')
+</script>
+
+<template>
+  <van-tabbar v-model="active">
+    <van-tabbar-item icon="home-o" name="discover">discover</van-tabbar-item>
+    <van-tabbar-item icon="search" dot name="plan">plan</van-tabbar-item>
+    <van-tabbar-item icon="friends-o" badge="5" name="record">record</van-tabbar-item>
+    <van-tabbar-item icon="setting-o" badge="20" name="profile">profile</van-tabbar-item>
+    <van-tabbar-item icon="setting-o" badge="20" name="shop">shop</van-tabbar-item>
+  </van-tabbar>
+</template>
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import '@amap/amap-jsapi-types'
 
 import { createApp } from 'vue'
 import ArcoVue from '@arco-design/web-vue'
+import Vant from 'vant'
 // 额外引入图标库
 import ArcoVueIcon from '@arco-design/web-vue/es/icon'
 import '@arco-design/web-vue/dist/arco.css'
+import 'vant/lib/index.css'
 import { createPinia } from 'pinia'
 
 import './scss/app.scss'
@@ -20,6 +22,9 @@ import LayoutMobileMain from '@/layout/mobile/LayoutMain.vue'
 app.use(createPinia())
 app.use(router)
 app.use(ArcoVue)
+app.use(Vant)
+// Lazyload 指令需要单独进行注册
+// app.use(Vant.Lazyload)
 app.use(ArcoVueIcon)
 app.component('layout-default', LayoutDefault)
 app.component('layout-a', LayoutA)
