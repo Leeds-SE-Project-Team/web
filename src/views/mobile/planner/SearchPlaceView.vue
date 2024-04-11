14157d30dff5461a3d185641aa665e588582745a
wmzspace <sc21m2w@leeds.ac.uk>
2024-04-11 10:52:43 +0800

    feat: update search page

--- src/views/mobile/planner/SearchPlaceView.vue
+++ src/views/mobile/planner/SearchPlaceView.vue
@@ -1,7 +1,35 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import useLoading from '@/hooks/loading'
 
 const searchInput = ref('')
+const recentSearchList = ref(['Southwest Jiaotong University', 'Chunxi Road'])
+
+const loadingObjPOI = useLoading()
+const finishedPOI = ref(false)
+const onLoadPOI = () => {
+  setTimeout(() => {
+    for (let i = 0; i < 5; i++) {
+      poiList.value.push({ icon: 'location', title: `Collection ${poiList.value.length + 1}` })
+    }
+
+    // 加载状态结束
+    loadingObjPOI.setLoading(false)
+
+    // 数据全部加载完成
+    if (poiList.value.length >= 20) {
+      finishedPOI.value = true
+    }
+  }, 1000)
+}
+
+const poiList = ref([
+  { icon: 'youzan-shield', title: 'Highlights' },
+  { icon: 'star', title: 'Starred Places' },
+  { icon: 'shop', title: 'Shopping Market' },
+  { icon: 'video', title: 'Cinemas' },
+  { icon: 'card', title: 'Banks' }
+])
 </script>
 
 <template>
@@ -43,12 +71,38 @@ const searchInput = ref('')
       </van-grid-item>
     </van-grid>
     <van-divider class="divider-1"></van-divider>
-    <div class="recent-search">
-      <p class="recent-search-title">Recent searches</p>
-      <!--      <div style="padding: 10px 0 10px 0">-->
-      <!--        <van-icon name="underway-o" size="21"/>-->
-      <!--        <span style="">South West Jiaotong University</span>-->
-      <!--      </div>-->
+    <div class="search-content-area">
+      <p class="search-content-area-title">Recent searches</p>
+      <van-list class="search-content-area-list van-hairline--bottom" finished-text="loaded all">
+        <van-cell v-for="(item, idx) in recentSearchList" :key="idx" :center="true">
+          <template #icon>
+            <van-icon class="search-content-area-list-icon" name="underway-o"></van-icon>
+          </template>
+          <template #title>
+            <span class="search-content-area-content van-hairline--bottom">{{ item }}</span>
+          </template>
+        </van-cell>
+      </van-list>
+      <van-divider class="divider-1"></van-divider>
+    </div>
+    <div class="search-content-area">
+      <p class="search-content-area-title">SHOW PLACES ON MAP</p>
+      <van-list
+        v-model:loading="loadingObjPOI.loading.value"
+        :finished="finishedPOI"
+        @load="onLoadPOI"
+        class="search-content-area-list van-hairline--bottom"
+        finished-text="loaded all"
+      >
+        <van-cell v-for="(item, idx) in poiList" :key="idx" :center="true">
+          <template #icon>
+            <van-icon class="search-content-area-list-icon" :name="item.icon"></van-icon>
+          </template>
+          <template #title>
+            <span class="search-content-area-content">{{ item.title }}</span>
+          </template>
+        </van-cell>
+      </van-list>
     </div>
   </div>
 </template>
